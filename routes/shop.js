40f58de7e577cba240521d6aa59943499e825616
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2024-05-31 10:56:06 +0100

    chore: Install templating engines

--- routes/shop.js
+++ routes/shop.js
@@ -4,8 +4,13 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const adminData = require("./admin");
+
 router.get("/", (req, res, next) => {
+  console.log("shop.js", adminData.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
+
+  // res.send(`<html><body><p>${adminData.products[0]?.title}</p></body> </html>`);
 });
 
 module.exports = router;
