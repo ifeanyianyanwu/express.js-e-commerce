baeafaf0fa29263545912dd85f371a4b28372da6
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2024-06-03 14:50:32 +0100

    feat: Implement new routes and controllers

--- models/product.js
+++ models/product.js
@@ -1,35 +1,40 @@
-const fs = require("fs");
-const path = require("path");
-const rootDir = require("../util/path");
+const fs = require('fs');
+const path = require('path');
 
-const p = path.join(rootDir, "data", "products.json");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const getProductsFromFile = (callBack) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return callBack([]);
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    callBack(JSON.parse(fileContent));
   });
 };
 
 module.exports = class Product {
-  constructor(product) {
-    this.title = product.title;
-    this.price = product.price;
-    this.description = product.description;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
   }
 
-  static fetchAll(callBack) {
-    getProductsFromFile(callBack);
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
